6b9a7c6c7d42a3d4ae8ec3a877c0c9f847ab9641
SantaMinus <santaminus@gmail.com>
2015-08-22 20:06:16 +0300

    Add accountactivations

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -22,7 +22,7 @@ module SessionsHelper
 			@current_user ||= User.find_by(id: user_id)
 		elsif (user_id = cookies.signed[:user_id])
 			user = User.find_by(id: user_id)
-			if user && user.authenticated?(cookies[:remember_token])
+			if user && user.authenticated?(:remember, cookies[:remember_token])
 				log_in user
 				@current_user = user
 			end
@@ -46,19 +46,6 @@ module SessionsHelper
 		forget(current_user)
 		session.delete(:user_id)
 		@current_user = nil
-	end
-
-	# Returns the user corresponding to the remember token cookie.
-	def current_user
-		if (user_id = session[:user_id])
-			@current_user ||= User.find_by(id: user_id)
-		elsif (user_id = cookies.signed[:user_id])
-			user = User.find_by(id: user_id)
-			if user && user.authenticated?(cookies[:remember_token])
-				log_in user
-				@current_user = user
-			end
-		end
 	end
 
 	# Redirects to stored location (or to the default).
