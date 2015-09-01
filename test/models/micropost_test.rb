4223f48aebfd87394ea5ea44b7c1e9bebc4ce93f
SantaMinus <santaminus@gmail.com>
2015-09-01 10:27:40 +0300

    Error in microposts.yml

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,13 +1,10 @@
 require 'test_helper'
 
-class MicropostTest < ActiveRecord::Base
-  belongs_to :user
-  validates :user_id, presence: true
+class MicropostTest < ActiveSupport::TestCase
 
   def setup
 	@user = users(:katia)
-	# This code is not idiomatically correct.
-	@micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+	@micropost = @user.microposts.build(content: "Lorem ipsum")
   end
 
   test "should be valid" do
@@ -17,5 +14,19 @@ class MicropostTest < ActiveRecord::Base
   test "user id should be present" do
 	@micropost.user_id = nil
 	assert_not @micropost.valid?  
+  end
+
+  test "content should be present" do
+	@micropost.content = " "
+	assert_not @micropost.valid?
+  end
+
+  test "content should be at most 140 characters" do
+	@micropost.content = "a" * 141
+	assert_not @micropost.valid?
+  end
+
+  test "order should be most recent first" do
+	assert_equal microposts(:most_recent), Micropost.first
   end
 end
